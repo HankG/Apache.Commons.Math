1026eef18011b74509606b48881a19753ae05b84
Mauro Franceschini <mauro.franceschini@gmail.com>
2011-11-17 08:51:44 +0100

    Added support for IField and IFieldElement.

---
+++ Apache.Commons.Math/IField.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apache.Commons.Math
+{
+    /// <summary>
+    /// Interface representing an field.
+    /// <para>Classes implementing this interface will often be singletons.</para>
+    /// </summary>
+    /// <seealso cref="http://mathworld.wolfram.com/Field.html"/>
+    /// <typeparam name="T">The type of the field elements.</typeparam>
+    public interface IField<T>
+    {
+        /// <summary>
+        /// Get the additive identity for the field.
+        /// <para>The additive identity is the element <em>e<sub>0</sub></em> of the field such that
+        /// for all elements <em>a</em> of the field, the equalities <em>a + e<sub>0</sub> = e<sub>0</sub> + a = a</em>
+        /// hold.</para>
+        /// </summary>
+        T Zero { get; }
+
+        /// <summary>
+        /// Get the multiplicative identity for the field.
+        /// <para>The multiplicative identity is the element <em>e<sub>1</sub></em> of the field such that
+        /// for all elements <em>a</em> of the field, the equalities <em>a &times; e<sub>1</sub> =
+        /// e<sub>1</sub> &times; a = a</em> hold.</para>
+        /// </summary>
+        T One { get; }
+
+        /// <summary>
+        /// Retrieve the runtime type of the <see cref="FieldElement"/>
+        /// </summary>
+        Type RuntimeType { get; }
+    }
+}
---
+++ Apache.Commons.Math/IFieldElement.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apache.Commons.Math
+{
+    /// <summary>
+    /// Interface representing field elements.
+    /// </summary>
+    /// <see cref="IField"/>
+    /// <seealso cref="http://mathworld.worlfram.com/Field.html"/>
+    /// <typeparam name="T">The type of the field elements.</typeparam>
+    public interface IFieldElement<T>
+    {
+        /// <summary>
+        /// Compute <c>this + a</c>.
+        /// </summary>
+        /// <param name="a">Element to add.</param>
+        /// <returns>A new element representing <c>this + a</c>.</returns>
+        T Add(T a);
+
+        /// <summary>
+        /// Compute <c>this - a</c>.
+        /// </summary>
+        /// <param name="a">Element to subtract.</param>
+        /// <returns>A new element representing <c>this - a</c>.</returns>
+        T Subtract(T a);
+
+        /// <summary>
+        /// Returns the additive inverse of <c>this</c> element.
+        /// </summary>
+        /// <returns>The opposite of <c>this</c>.</returns>
+        T Negate();
+
+        /// <summary>
+        /// Compute <c>n &times; this</c>.
+        /// <para>Multiplication by an integer number is defined as the folling sum:
+        /// <center><c>n &times; this = &sum;<sub>i=1</sub><sup>n</sup>this</c></center></para>
+        /// </summary>
+        /// <param name="n">Number of times <c>this</c> must be added to itself.</param>
+        /// <returns>A new element representing n &times; this.</returns>
+        T Multiply(int n);
+
+        /// <summary>
+        /// Compute <c>this &times; a</c>.
+        /// </summary>
+        /// <param name="a">Element to multiply</param>
+        /// <returns>A new element representing <c>this &times; a</c>.</returns>
+        T Multiply(T a);
+
+        /// <summary>
+        /// Compute <c>this &divide; a</c>.
+        /// </summary>
+        /// <param name="a">Element to divide for.</param>
+        /// <returns>A new element representing <c>this &divide; a</c>.</returns>
+        /// <exception cref="ArithmeticException">If <paramref name="a"/> is the zero of the additive operation (i.e. additive identity).</exception>
+        T Divide(T a);
+
+        /// <summary>
+        /// Return the multiplicative inverse of <c>this</c>.
+        /// </summary>
+        /// <returns>The inverse of <c>this</c>.</returns>
+        T Reciprocal();
+
+        /// <summary>
+        /// Get the <see cref="IField"/> to which the instance belongs.
+        /// </summary>
+        IField<T> Field { get; }
+    }
+}
